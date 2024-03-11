a5006c918daaad044778d7dfca8b2cf95c6fa421
Black Lover <158266786+BLACKLOVER15@users.noreply.github.com>
2024-03-11 10:45:17 +0530

    Update queue.py

--- SHUKLAMUSIC/utils/inline/queue.py
+++ SHUKLAMUSIC/utils/inline/queue.py
@@ -66,8 +66,8 @@ def queue_back_markup(_, CPLAY):
 def aq_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="🤍 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 🖤", url="https://t.me/+uF_GVlNuJ_dkZGVl"),
-            InlineKeyboardButton(text="🖤 𝐎𝐖𝐍𝐄𝐑 🤍", url="https://t.me/l_MR_ll_KING_l"),
+            InlineKeyboardButton(text="🤍 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 🖤", url="https://t.me/All_Exam_Music_Support"),
+            InlineKeyboardButton(text="🖤 𝐎𝐖𝐍𝐄𝐑 🤍", url="https://t.me/Blacklover_uff"),
         ],
     ]
     return buttons
