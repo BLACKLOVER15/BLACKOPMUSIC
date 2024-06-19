b1a2e31f3b483c3476fc7956133b270e8ba47c4d
Black Lover <158266786+BLACKLOVER15@users.noreply.github.com>
2024-06-19 06:02:03 +0530

    Update start.py

--- SHUKLAMUSIC/utils/inline/start.py
+++ SHUKLAMUSIC/utils/inline/start.py
@@ -35,7 +35,7 @@ def private_panel(_):
         [
             InlineKeyboardButton(text="🍬 sᴜᴘᴘᴏꝛᴛ 🍬", url=config.SUPPORT_CHAT),
             InlineKeyboardButton(text="🍹 ᴜᴘᴅᴀᴛᴇ 🍹", url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text="🎖ᴏᴡɴᴇʀ🎖", url=f"https://t.me/DEWANA_HOON"),
+            InlineKeyboardButton(text="🎖ᴏᴡɴᴇʀ🎖", url=f"https://t.me/VENOM_PRATAP"),
         ],
         [
             InlineKeyboardButton(text="🏩 ʜᴇʟᴘ ᴧиᴅ ᴄᴏᴍᴍᴧɴᴅs 🏩", callback_data="settings_back_helper")
